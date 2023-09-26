e4fb5255b8b7be33d6b8d7f9c4ee6786bed2ede0
yves137 <oneg2981@gmail.com>
2023-09-26 15:29:33 +0200

    update README

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const animals=['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
 function App() {
   return (
     <ul className="ml-10 mt-10 ">
-      {animals.map((animal,i)=><li key={i}>{animal}</li>)}
+      {animals.map((animal,i)=><li key={i}><span className="text-4xl font-bold">.</span> {animal}</li>)}
     </ul>
   );
 }
